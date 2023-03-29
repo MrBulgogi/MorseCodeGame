b5632f856e5556f5161358ec6a48da7d7cc6aeb8
hanburyc <hanburyc@tcd.ie>
2023-03-29 15:41:31 +0100

    added some C functions that may be useful,some changes to arm  Please enter the commit message for your changes. Lines starting

--- assign02.c
+++ assign02.c
@@ -2,13 +2,6 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-int startbtn;
-int endbtn;
-int timebtn;
-int startgap;
-int endgap;
-int timegap;
-
 // Declare the main assembly code entry point.
 void main_asm();
 
@@ -41,46 +34,74 @@ void asm_gpio_set_irq2(uint pin) {
     gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_RISE, true);
 }
 
-void printdash(){
-    printf("-");
+void morsematching(){
+    
+
+    OUPUT =
 }
 
-void printdot(){
-    printf(".");
+void printuser(){
+    morsematching();
+    printf("\33[2K\r")
+    printf("%s | %s", OUPUT, INPUT)
 }
-
-void printspace(){
-    if (i < 2){
-        printf(" ");
+void stringadd(msg){
+    if(msg == "dot"){
+        strcat(INPUT, ".");
+        printuser();
     }
+    else if(msg = "dash"){
+        strcat(INPUT, "-");
+        printuser();
+    }
+    else if(msg = "space"){
+        strcat(INPUT, " ");
+        printuser();
+    }
+    else if(msg == "enter"){
+        strcpy(INPUT, "");
+        printuser();
+    }
+}
+
+void stringadd(msg){
+    if(msg == "dot"){
+        strcat(INPUT, ".");
+    }
+    else if(msg = "dash"){
+        strcat(INPUT, "-");
+    }
+    else if(msg = "space"){
+        strcat(INPUT, " ");
+    }
+    else if(msg == "enter"){
+        strcpy(INPUT, "");
+    }
+}
+
+bool entermorse(){
+    return True;
 }
 void watchdog(){
     
 }
 
-
-// Main entry point of the application
-int main() {
+void homescreen(){
     FILE *fptr;  
     fptr = fopen("ascii_art.txt", "r"); 
     printf("%c", fgetc(fptr));
     printf("Enter Morse To Start Level\n< . - - - - > | Level 01 | Chars | EASY\n< . . - - - > | Level 02 | Chars | HARD\n< . . . - - > | Level 03 | Words | EASY\n< . . . . - > | Level 04 | Words | HARD\n\n");  // Basic print to console
-    stdio_init_all();  
-    //clrscr();
-    /*while (true){
-        if (timebtn <= 1000000){
-            printf(".");
-        }
-        else if (timebtn <= 3000000 && timebtn >= 1000000){
-            printf("-");
-        }
+}
 
-        if (timegap >= 2500000 && timegap <= 3500000){
-            printf(" ");
-        }
-        else if (timegap )
-    }
+void homescreenclr(){
+    clrscr();
+}
 
-    }*/
+
+// Main entry point of the application
+int main() {
+    stdio_init_all();
+    char[20] INPUT = "";
+    char[1] OUTPUT = "";
     return 0;                      // Application return code
 }
