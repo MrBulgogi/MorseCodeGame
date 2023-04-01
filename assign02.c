620748e5d7ee372a0a02a1fb64f11aad72bbf2b1
Cian Twomey <citwomey@tcd.ie>
2023-04-01 19:41:26 +0100

    Implement level1-4 Print out character/word with/without morse equivalent Each function returns morse equivalent of required character/word

--- assign02.c
+++ assign02.c
@@ -12,150 +12,17 @@
 #define NUM_PIXELS 1  // There is 1 WS2812 device in the chain
 #define WS2812_PIN 28 // The GPIO pin that the WS2812 connected
 #define ARRAY_SIZE 38
+#define MAX_STRING 30
 
 int lives;
 int count; // count will be incremented/decremented if a player gets the correct answer
 
 char *translate(char c);
-
-void checkEntry(char C, char *string, int lives)
-{
-    char *morse = translate(C);
-    if (C == 'A')
-    {
-        if (strcmp(morse, ".-") == 0)
-        {
-            printf("CORRECT!\n");
-            if (lives < 3)
-            {
-                lives++;
-            }
-        }
-        else
-        {
-            printf("INCOORECT\n");
-            if (lives > 0)
-            {
-                lives--;
-            }
-        }
-    }
-    printf("You have %d lives left!\n", lives);
-}
-
-// char* translation(char original){
-//     if(original == 'A'){
-//         char a[6] = '.-';
-//     }
-//     if(original == 'B'){
-//         char a[6] = '-...';
-//     }
-//     if(original == 'C'){
-//         char a[6] = '-.-.';
-//     }
-//     if(original == 'D'){
-//         char a[6] = '-..';
-//     }
-//     if(original == 'E'){
-//         char a[6] = '.';
-//     }
-//     if(original == 'F'){
-//         char a[6] = '..-.';
-//     }
-//     if(original == 'G'){
-//         char a[6] = '--.';
-//     }
-//     if(original == 'H'){
-//         char a[6] = '....';
-//     }
-//     if(original == 'I'){
-//         char a[6] = '..';
-//     }
-//     if(original == 'J'){
-//         char a[6] = '.---';
-//     }
-//     if(original == 'K'){
-//         char a[6] = '-.-';
-//     }
-//     if(original == 'L'){
-//         char a[6] = '.-..';
-//     }
-//     if(original == 'M'){
-//         char a[6] = '--';
-//     }
-//     if(original == 'N'){
-//         char a[6] = '-.';
-//     }
-//     if(original == 'O'){
-//         char a[6] = '---';
-//     }
-//     if(original == 'P'){
-//         char a[6] = '.--.';
-//     }
-//     if(original == 'Q'){
-//         char a[6] = '--.-';
-//     }
-//     if(original == 'R'){
-//         char a[6] = '.-.';
-//     }
-//     if(original == 'S'){
-//         char a[6] = '...';
-//     }
-//     if(original == 'T'){
-//         char a[6] = '-';
-//     }
-//     if(original == 'U'){
-//         char a[6] = '..-';
-//     }
-//     if(original == 'V'){
-//         char a[6] = '...-';
-//     }
-//     if(original == 'W'){
-//         char a[6] = '.--';
-//     }
-//     if(original == 'X'){
-//         char a[6] = '-..-';
-//     }
-//     if(original == 'Y'){
-//         char a[6] = '-.--';
-//     }
-//     if(original == 'Z'){
-//         char a[6] = '--..';
-//     }
-//     if(original == '0'){
-//         char a[6] = '-----';
-//     }
-//     if(original == '1'){
-//         char a[6] = '.----';
-//     }
-//     if(original == '2'){
-//         char a[6] = '..---';
-//     }
-//     if(original == '3'){
-//         char a[6] = '...--';
-//     }
-//     if(original == '4'){
-//         char a[6] = '....-';
-//     }
-//     if(original == '5'){
-//         char a[6] = '.....';
-//     }
-//     if(original == '6'){
-//         char a[6] = '-....';
-//     }
-//     if(original == '7'){
-//         char a[6] = '--...';
-//     }
-//     if(original == '8'){
-//         char a[6] = '---..';
-//     }
-//     if(original == '9'){
-//         char a[6] = '----.';
-//     }
-//     return *a;
-// }
-
-void outputChar();
+void checkEntry(char C, char *string, int lives);
+char *level1();
+char *level2();
+char *level3();
+char *level4();
 void setLED(int lives);
 /**
  * @brief Wrapper function used to call the underlying PIO
@@ -228,70 +95,103 @@ void asm_gpio_set_irq(uint pin)
     printf("TIME: %d\n", total);
 }
 
-/*
- * Main entry point for the code - simply calls the main assembly function.
- */
-int main()
+char alphabet[ARRAY_SIZE] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                             'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' '};
+
+char words[][20] = {"HELLO", "WORLD", "MORSE", "CODE", "MICRO", "COMP", "PICO", "WATER", "BREAD", "BOARD", "CAR", "COPY", "PASTE"};
+
+char *level1()
 {
-    stdio_init_all();
-    // main_asm();
-    //  Initialise the PIO interface with the WS2812 code
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &ws2812_program);
-    ws2812_program_init(pio, 0, offset, WS2812_PIN, 800000, IS_RGBW);
-
-    printf("NEW\n");
-    outputChar();
-    printf("\n-------------------------\n");
-    char *morse = translate('A');
-    printf("\nqqqqqqqqqqq%sqqqqqqqqqqq\n", morse);
-
-    // char myString[] = ".-";
-    // char a = 'A';
-    // char myStringB[] = "no";
-
-    lives = 3;
-    // checkEntry(a, myString, lives);
-    // checkEntry(a, myStringB, lives);
-    while (true)
-    {
-        setLED(0);
-        sleep_ms(500);
-        setLED(1);
-        sleep_ms(500);
-        setLED(2);
-        sleep_ms(500);
-        setLED(3);
-        sleep_ms(500);
-    }
-
-    // Do forever...
-    // while (true)
-    // {
-
-    //     // Set the color to red at half intensity
-    //     put_pixel(urgb_u32(0x7F, 0x00, 0x00));
-    //     sleep_ms(500);
-
-    //     // Set the color to green at half intensity
-    //     put_pixel(urgb_u32(0x00, 0x7F, 0x00));
-    //     sleep_ms(500);
-
-    //     // Set the color to blue at half intensity
-    //     put_pixel(urgb_u32(0x00, 0x00, 0x7F));
-    //     sleep_ms(500);
-    // }
-    return 0;
+    printf("\nLEVEL 1\n-------\nIn this level your task is to enter the morse code corresponding to the given character\n");
+    printf("You are given both a character and the morse equivalent below \n");
+    int random = rand() % (ARRAY_SIZE - 1);
+    char c = alphabet[random];
+    char *s = translate(c);
+    printf("\nCharacter: %c\n", c);
+    printf("Morse equivalent of %c: %s \n", c, s);
+    return s;
 }
 
-char alphabet[ARRAY_SIZE] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                             'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
-void outputChar()
+char *level2()
 {
-    int random = rand() % ARRAY_SIZE;
+    printf("\nLEVEL 2\n-------\nIn this level your task is to enter the morse code corresponding to the given character\n");
+    int random = rand() % (ARRAY_SIZE - 1);
     char c = alphabet[random];
-    printf("\nCharacter to show in Morse: %c\n", c);
+    char *s = translate(c);
+    printf("\nCharacter: %c\n", c);
+    return s;
+}
+
+char *level3()
+{
+    printf("\nLEVEL 3\n-------\nIn this level your task is to enter the morse code corresponding to the given word\n");
+    printf("You are given both a word and the morse equivalent below \n");
+    int random = rand() % (12);
+    char *s = words[random];
+    char *morse;
+    char *trans = malloc(sizeof(char) * (MAX_STRING));
+    if (trans == NULL)
+    {
+        printf("\nError allocating memory for 'trans'\n");
+    }
+    trans[0] = '\0';
+    for (int i = 0; i < strlen(s); i++)
+    {
+        char c;
+        c = s[i];
+        morse = translate(c);
+        char space = ' ';
+        strncat(trans, morse, 4);
+        morse = translate(space);
+        strncat(trans, morse, 1);
+    }
+    char *newTrans = malloc(sizeof(char) * (MAX_STRING));
+    if (newTrans == NULL)
+    {
+        printf("\nError allocating memory for 'newTrans'\n");
+    }
+    newTrans[0] = '\0';
+    strncpy(newTrans, trans, strlen(trans) - 1);
+    newTrans[strlen(trans) - 1] = '\0';
+
+    printf("\nWord: %s\n", s);
+    printf("Morse equivalent of %s: %s \n", s, newTrans);
+    return newTrans;
+}
+
+char *level4()
+{
+    printf("\nLEVEL 4\n-------\nIn this level your task is to enter the morse code corresponding to the given word\n");
+    int random = rand() % (12);
+    char *s = words[random];
+    char *morse;
+    char *trans = malloc(sizeof(char) * (MAX_STRING));
+    if (trans == NULL)
+    {
+        printf("\nError allocating memory for 'trans'\n");
+    }
+    trans[0] = '\0';
+    for (int i = 0; i < strlen(s); i++)
+    {
+        char c;
+        c = s[i];
+        morse = translate(c);
+        char space = ' ';
+        strncat(trans, morse, 4);
+        morse = translate(space);
+        strncat(trans, morse, 1);
+    }
+    char *newTrans = malloc(sizeof(char) * (MAX_STRING));
+    if (newTrans == NULL)
+    {
+        printf("\nError allocating memory for 'newTrans'\n");
+    }
+    newTrans[0] = '\0';
+    strncpy(newTrans, trans, strlen(trans) - 1);
+    newTrans[strlen(trans) - 1] = '\0';
+
+    printf("\nWord: %s\n", s);
+    return newTrans;
 }
 
 void setLED(int lives)
@@ -321,7 +221,7 @@ void setLED(int lives)
 
 char *translate(char C)
 {
-    int i;
+    int i = 0;
     // all possible morse outputs
     char *morse[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
                      "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
@@ -349,7 +249,7 @@ char *translate(char C)
     }
     // if ascii value of C is between ascii val of 0 & 9 set index to
     // ascii of C - ascii of 0 and add 26 to skip the alphabet chars
-    else if (C <= lastNum && C <= firstNum)
+    else if (C <= lastNum && C >= firstNum)
     {
         i = (int)C - firstNum + 26;
     }
@@ -366,8 +266,64 @@ char *translate(char C)
         return NULL;
     }
 }
-// get the C part of reading correct
 
-// take times from arm
-// convert to . or -
-//
+void checkEntry(char C, char *string, int lives)
+{
+    char *morse = translate(C);
+    if (C == 'A')
+    {
+        if (strcmp(morse, ".-") == 0)
+        {
+            printf("CORRECT!\n");
+            if (lives < 3)
+            {
+                lives++;
+            }
+        }
+        else
+        {
+            printf("INCOORECT\n");
+            if (lives > 0)
+            {
+                lives--;
+            }
+        }
+    }
+    printf("You have %d lives left!\n", lives);
+}
+
+/*
+ * Main entry point for the code - simply calls the main assembly function.
+ */
+int main()
+{
+    stdio_init_all();
+    // main_asm();
+    //  Initialise the PIO interface with the WS2812 code
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &ws2812_program);
+    ws2812_program_init(pio, 0, offset, WS2812_PIN, 800000, IS_RGBW);
+    printf("\n-------------------------\n");
+    printf("\n-------------------------\n");
+    printf("\n-------------------------\n");
+    level1();
+    level2();
+    level3();
+    level4();
+    printf("\n-------------------------\n");
+    printf("\n-------------------------\n");
+    printf("\n-------------------------\n");
+
+    while (true)
+    {
+        setLED(0);
+        sleep_ms(500);
+        setLED(1);
+        sleep_ms(500);
+        setLED(2);
+        sleep_ms(500);
+        setLED(3);
+        sleep_ms(500);
+    }
+    return 0;
+}
